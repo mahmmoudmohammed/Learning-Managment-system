3bc2b4f84366c35a890cdb188fd59c6a8de926a8
mahmoud <mahmmoudmohamed2020@gmail.com>
2023-06-21 00:28:21 +0200

    Create Project structure

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Controllers\Api\Modules\Answers\Answer;
+use App\Policies\AnswerPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+         Answer::class => AnswerPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
